d058c3e6c248f6822c7b1b08e0c76031a88841dd
Antonio Scaldaferri <s210722@studenti.polito.it>
2021-07-14 17:20:29 +0200

    Wood 2 Solution

---
+++ wood2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+int main()
+{
+    // game loop
+    while (1) {
+        int x;
+        int y;
+        int nextCheckpointX; // x position of the next check point
+        int nextCheckpointY; // y position of the next check point
+        int nextCheckpointDist; // distance to the next checkpoint
+        int nextCheckpointAngle; // angle between your pod orientation and the direction of the next checkpoint
+        cin >> x >> y >> nextCheckpointX >> nextCheckpointY >> nextCheckpointDist >> nextCheckpointAngle; cin.ignore();
+        int opponentX;
+        int opponentY;
+        cin >> opponentX >> opponentY; cin.ignore();
+
+        // Write an action using cout. DON'T FORGET THE "<< endl"
+        // To debug: cerr << "Debug messages..." << endl;
+
+
+        // You have to output the target position
+        // followed by the power (0 <= thrust <= 100)
+        // i.e.: "x y thrust"
+
+        int thrust = (nextCheckpointAngle > 90  || nextCheckpointAngle < -90) ? 0 : 100;
+        
+        cout << nextCheckpointX << " 0" << nextCheckpointY << " " <<  thrust << endl;
+    }
+}
